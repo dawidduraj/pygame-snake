cd76980ed1966a944eb8467c9d3671626a24a4df
Dawid Duraj <dawid.duraj@deepstrm.com>
2022-09-26 21:12:06 +0200

    Setup pygame

---
+++ game.py
@@ -0,0 +1,11 @@
+import pygame
+
+# Variables/Constants
+SIZE = 500
+
+
+# setup
+pygame.init()
+window = pygame.display.set_mode((SIZE,SIZE))
+clock = pygame.time.Clock()
+pygame.display.set_caption("Snake 🐍")
